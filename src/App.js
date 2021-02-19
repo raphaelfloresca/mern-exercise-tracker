371662f2311e06287c36371a8f79c3ae721b6fa0
Raphael Angelo Floresca <florescaraphangelo@gmail.com>
2021-02-19 16:40:46 +0800

    Finished create exercise log frontend

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/navbar.component";
-// import ExercisesList from "./components/exercises-list.component";
-// import EditExercise from "./components/edit-exercise.component";
-// import CreateExercise from "./components/create-exercise.component";
-// import CreateUser from "./components/create-user.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component"; 
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
@@ -13,13 +13,13 @@ function App() {
       <div className="container">
         <Navbar />
         <br />
-        {/* <Route path="/" exact component={ExercisesList} /> */}
-        {/* <Route path="/edit/:id" component={EditExercise} /> */}
-        {/* <Route path="/create" component={CreateExercise} /> */}
-        {/* <Route path="/user" component={CreateUser} /> */}
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
       </div>
     </Router>
   );
 }
 
-export default App;
+export default App; 
--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-export default class ExercisesList extends Component {
+export default class CreateUser extends Component {
     render() {
         return(
             <div>
-                <p>You are on the Exercises List component!</p>
+                <p>You are on the Create User component!</p>
             </div>
         )
     }
